517e429508affe270bcbf3f0a857eaa3be3adb13
yeonho Park <whwlsvy12@naver.com>
2019-03-23 12:57:26 +0900

    modify binary.java

--- 문제/binary.java
+++ 문제/binary.java
@@ -7,8 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        ArrayList list = new ArrayList();
-        int na, mo = 2;
+        ArrayList list = new ArrayList(); // 2진수를 저장한 리스트
 
         if (n == 1) { // 입력받은 숫자가 1이면 1을 출력
             System.out.println(1);
@@ -16,16 +15,16 @@ public class Main {
         }
 
         while (true) {
-            list.add(n % 2);
-            n /= 2;
+            list.add(n % 2); // 2진수는 입력받은 숫자의 나머지 값
+            n /= 2; // 한번 나눴으면 n 값은 다시 2로 나눈 몫으로 설정
 
-            if (n == 1) {
+            if (n == 1) { // 마지막으로 남은 몫이 1인 경우 1을 저장하고 while문을 빠져 나온다.
                 list.add(1);
                 break;
             }
         }
 
-        for (int i = list.size() - 1; i >= 0; i--) {
+        for (int i = list.size() - 1; i >= 0; i--) { // 저장된 2진수를 역으로 출력
             System.out.print(list.get(i));
         }
     }
