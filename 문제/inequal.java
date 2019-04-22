ef34e26529272db8a16c547cb3096addedac973d
yeonho Park <whwlsvy12@naver.com>
2019-04-22 14:42:04 +0900

    modify inequal

--- 문제/inequal.java
+++ 문제/inequal.java
@@ -2,83 +2,107 @@ import java.util.*;
 
 public class Main {
 
-    static boolean isFindMax = false;
-    static boolean isFindMin = false;
-    static char[] arr;
-    static List<Integer> list = new LinkedList<Integer>();
+    static List<Integer> numList = new LinkedList<Integer>(); // 사용되는 숫자들을 저장
+    static List<Integer> check = new LinkedList<Integer>(); // 사용한 숫자를 확인
+    static List<Character> operatorList = new LinkedList<Character>(); // 연산자를 저장
+    static boolean isFindMax = false; //
+    static boolean isMinFind = false;
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        arr = new char[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.next().charAt(0);
+        for (int i = 0; i < n; i++) { // 입력받은 연산자를 저장
+            operatorList.add(sc.next().charAt(0));
         }
 
-        // 0 : max , 1 : min
-        search(9, n + 1, 0);
-        list.clear();
-        // search(0,n+1,1);
+        search(9, n + 1, 0); // 최대값 찾기
+        numList.clear();
+        check.clear();
+        search(0, n + 1, 1); // 최소값 찾기
+
     }
 
-    static void search(int start, int count, int division) {
-        if (count == 0) {
-            if (division == 0 && !isFindMax) { // 최대값
-                check(division);
-            } else {
+    static void search(int n, int count, int division) { // 모든 가능한 숫자조합을 찾음 , division=0이면 최대값, division=1이면 최소값
+        if (count == 0) { // 기저조건은 숫자를 출력할 수 있는 횟수가 0일때
+            if (division == 0 && !isFindMax) { // 숫자의 조합을 완성하고 아직 최대값을 찾지 않은 경우 print()함수 실행
+                print(division);
+            }
 
+            if (division == 1 && !isMinFind) {
+                print(division);
             }
         } else {
-            if (division == 0 && !isFindMax) { // 최소값
-                for (int i = start; i >= 0; i--) {
-                    // if(list.contains(i)){
-                    // continue;
-                    // }
-                    list.add(i);
-                    search(start, count - 1, 0);
-                    list.remove(list.size() - 1);
+            if (division == 0 && !isFindMax) {
+                for (int i = 9; i >= 0; i--) {
+                    if (!check.contains(i)) { // 숫자의 조합에서 중복되는 숫자를 제외하기 위함
+                        numList.add(i); // 사용한 숫자는 리스트에 추가
+                        check.add(i); // 사용한 숫자를 표시
+                        search(i, count - 1, 0); // // 숫자의 조합을 찾기 위해 다시 호출, 하나의 숫자는 찾았기 때문에 count-1
+                        numList.remove(numList.size() - 1); // n번째 숫자에서 모든 숫자의 조합을 찾았으면 n번째 숫자는 제거
+                        check.remove(check.size() - 1); // 마찬가지로 check에서도 제거
+                    }
                 }
-            } else { // 최소값
+            }
 
+            if (division == 1 && !isMinFind) {
+                for (int i = 0; i <= 9; i++) {
+                    if (!check.contains(i)) {
+                        numList.add(i);
+                        check.add(i);
+                        search(i, count - 1, 1);
+                        numList.remove(numList.size() - 1);
+                        check.remove(check.size() - 1);
+                    }
+                }
             }
         }
     }
 
-    static void check(int diff) {
-        if (diff == 0) {
-            String s = "";
-            int arrIndex = 0;
-            int listIndex = 0;
-            int length = list.size() + arr.length;
+    static void print(int division) { // 찾은 숫자의 조합과 연산자를 사용해서 조건에 하는지 확인하는 함수
 
-            for (int i = 0; i < length; i++) {
-                if (i % 2 == 0) {
-                    s += list.get(listIndex) + "";
-                    listIndex++;
+        boolean maxCheck = true;
+        boolean minCheck = true;
+
+        for (int i = 0; i < operatorList.size(); i++) { // 8<9>7 에서 연산자 양옆으로 숫자들을 비교
+            int start = numList.get(i); // 연산자 왼쪽 숫자
+            int end = numList.get(i + 1);// 연산자 오른쪽 숫자
+            char operator = operatorList.get(i); // 연산자
+
+            if (operator == '>' && start <= end) { // 연산자가 '>'인 경우, 왼쪽이 무조건 커야하는데 왼쪽이 오른쪽보다 작거나 같으면 false
+                if (division == 0) {
+                    maxCheck = false;
                 } else {
-                    s += arr[arrIndex] + "";
-                    arrIndex++;
+                    minCheck = false;
                 }
+                break; // 하나라도 실패한 케이스가 있으면 바로 break;
             }
 
-            for (int i = 1; i < s.length(); i += 2) {
-                int start = list.get(i - 1);
-                int end = list.get(i + 1);
-                char c = s.charAt(i);
-
-                if (c == '>' && start > end) {
-                    return;
+            if (operator == '<' && start >= end) { // 연산자가 '<'인 경우, 오른쪽 무조건 커야하는데 오른쪽이 왼쪽보다 작거나 같으면 false
+                if (division == 0) {
+                    maxCheck = false;
+                } else {
+                    minCheck = false;
                 }
-                if (c == '<' && start < end) {
-                    return;
-                }
+                break;
             }
-            for (int i : list) {
+        }
+
+        if (division == 0 && maxCheck) { // 위의 조건을 모두 만족하면 현재 숫자들을 출력.
+            isFindMax = true;
+            for (int i : numList) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        if (division == 1 && minCheck) {
+            isMinFind = true;
+            for (int i : numList) {
                 System.out.print(i);
             }
         }
+
     }
 }
