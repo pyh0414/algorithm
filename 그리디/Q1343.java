eb89420e9d6cc6be7520d72b92600fc01214913b
yeonho Park <whwlsvy12@naver.com>
2019-06-21 14:23:42 +0900

    modify logic

--- 그리디/Q1343.java
+++ 그리디/Q1343.java
@@ -17,11 +17,6 @@ public class Main {
         char c;
         int count = 0;
 
-        // .을 만나서X수가 홀수이면 무조건 -1을 출력(예외, X인 경우는 바로 -1 )
-        // s==”X”이면 바로 -1
-        // X인 경우, count++( i==마지막 숫자 ? count가 홀수이면 -1 아니면 print)
-        // . 인 경우 , .을 만나서 count가 홀수이면 -1 아니면 print) result+”.”
-
         for (int i = 0; i < s.length(); i++) {
             c = s.charAt(i);
             if (c == 'X') {
@@ -42,12 +37,6 @@ public class Main {
                     print(count);
                     result += ".";
                     count = 0;
-                }
-                if (i == s.length() - 1) {
-                    if (count % 2 == 1) {
-                        System.out.println(-1);
-                        return;
-                    }
                 }
             }
         }
