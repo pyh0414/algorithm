5abedd0db75136e9b8651226f9577f62de2effaa
yeonho Park <whwlsvy12@naver.com>
2019-07-11 11:44:49 +0900

    add Heap

---
+++ 트리/Heap.java
@@ -0,0 +1,76 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int n, item;
+        Heap h = new Heap();
+        h.insertHeap(13);
+        h.insertHeap(8);
+        h.insertHeap(10);
+        h.insertHeap(15);
+        h.insertHeap(20);
+        h.insertHeap(19);
+
+        h.printHeap();
+        n = h.getHeapSize();
+
+        for (int i = 1; i <= n; i++) {
+            item = h.deleteHeap();
+            System.out.println("delete Item : " + item);
+        }
+
+    }
+}
+
+class Heap {
+    private int heapSize;
+    private int itemHeap[];
+
+    public Heap() {
+        heapSize = 0;
+        itemHeap = new int[50];
+    }
+
+    public int getHeapSize() {
+        return this.heapSize;
+    }
+
+    public void insertHeap(int item) {
+        int i = ++heapSize;
+        while ((i != 1) && (item > itemHeap[i / 2])) {
+            itemHeap[i] = itemHeap[i / 2];
+            i /= 2;
+        }
+        itemHeap[i] = item;
+    }
+
+    public int deleteHeap() {
+        int parent, child;
+        int item, temp;
+
+        item = itemHeap[1];
+        temp = itemHeap[heapSize--];
+        parent = 1;
+        child = 1;
+
+        while (child <= heapSize) {
+            if ((child < heapSize) && (itemHeap[child] < itemHeap[child + 1])) {
+                child++;
+            }
+            if (temp >= itemHeap[child]) {
+                break;
+            }
+            itemHeap[parent] = itemHeap[child];
+            parent = child;
+            child *= 2;
+        }
+        itemHeap[parent] = temp;
+        return item;
+    }
+
+    public void printHeap() {
+        System.out.println("\nHeap >>> ");
+        for (int i = 1; i <= heapSize; i++) {
+            System.out.print(itemHeap[i] + " ");
+        }
+    }
+}
