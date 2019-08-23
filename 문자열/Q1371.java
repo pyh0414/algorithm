9cca48d157f415b6d492280f2a173e9975435819
yeonho Park <whwlsvy12@naver.com>
2019-08-23 16:37:16 +0900

    add Q1371.java

---
+++ 문자열/Q1371.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+        int[] check = new int[26];
+
+        int max = 0;
+        int index;
+
+        List<Character> list = new ArrayList<Character>();
+
+        while (sc.hasNextLine()) {
+            str = sc.nextLine();
+            String[] arr = str.split(" ");
+
+            for (int j = 0; j < arr.length; j++) {
+                String s = arr[j];
+                for (int i = 0; i < s.length(); i++) {
+                    index = s.charAt(i) - 'a';
+                    check[index]++;
+                    if (check[index] > max) {
+                        max = check[index];
+                    }
+                }
+            }
+        }
+
+        for (int k = 0; k < 26; k++) {
+            if (check[k] == max) {
+                System.out.print((char) ('a' + k));
+            }
+        }
+
+    }
+}
