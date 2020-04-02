2be4495f11a17cc125d4227ff38655e0486e3e50
yeonho Park <whwlsvy12@naver.com>
2020-04-02 16:14:34 +0900

    add Q2193

---
+++ dp/Q2193.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    long long dp[91] = {0};
+
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n];
+    return 0;
+}
