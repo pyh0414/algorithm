55bbd14872f5a623b8b1bf828cc4af0194428b7a
yeonho Park <whwlsvy12@naver.com>
2020-04-05 00:40:44 +0900

    add Q1912

--- dp/Q1912.cpp
+++ dp/Q1912.cpp
@@ -11,31 +11,19 @@ int main()
     int arr[100001];
     int dp[100001];
 
-    bool allMinus = true;
     for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
-
-        if (arr[i] >= 0)
-        {
-            allMinus = false;
-        }
     }
 
     int temp = 0;
-    if (allMinus)
-    {
-        sort(arr, arr + n + 1);
-        cout << arr[n - 1];
-        return 0;
-    }
 
     dp[1] = arr[1];
     int result = dp[1];
 
     for (int i = 2; i <= n; i++)
     {
-        dp[i] = max(dp[i - 1] + arr[i], 0);
+        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
         result = max(result, dp[i]);
     }
 
