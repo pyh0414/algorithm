105b7aeb7023f1f8bd0b3313d32a40bd47384fa9
yeonho Park <whwlsvy12@naver.com>
2020-04-04 15:42:45 +0900

    add Q11055

---
+++ dp/Q11055.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[1001] = {0};
+    int dp[1001] = {0};
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int mx = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 1; j < i; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                sum = max(sum, dp[j]);
+            }
+        }
+        dp[i] = sum + arr[i];
+        mx = max(mx, dp[i]);
+    }
+
+    cout << mx;
+    return 0;
+}
